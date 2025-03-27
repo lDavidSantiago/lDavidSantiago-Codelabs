765a95caedc2df8fa8ee2a3b8375f153bda52837
lDavidSantiago <David.triana@correounivalle.edu.co>
2025-03-27 18:47:06 -0500

    Codelab RabbitMQ

---
+++ CodeLabs/Async con Spring Boot & RabbitMQ (Usando JSON)/pedido-service/src/main/java/dev/ldavidsantiago/pedidoservice/PedidoServiceApplication.java
@@ -0,0 +1,12 @@
+package dev.ldavidsantiago.pedidoservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PedidoServiceApplication.class, args);
+    }
+}
---
+++ CodeLabs/Async con Spring Boot & RabbitMQ (Usando JSON)/producto-service/src/main/java/dev/ldavidsantiago/productoservice/ProductoDTO.java
@@ -0,0 +1,22 @@
+package dev.ldavidsantiago.productoservice;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductoDTO {
+    private Long id;
+    private String nombre;
+    private Double precio;
+
+    @Override
+    public String toString() {
+        return "ProductoDTO{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
